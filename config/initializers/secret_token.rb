42565670cc8111d1a3ebc6827d97793cc81a9a1a
vidhyagp <vidhya.gp@gmail.com>
2014-02-16 21:11:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Planit::Application.config.secret_key_base = '8974b57d7dd4af2b17ced414c7cfa671acfc4357455aec8a52fc610816afe1d8859c8346d06e0b073ed5d7b9c67830e1f467f4583251628e2080fe99ca7a783d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Planit::Application.config.session_store :cookie_store, key: '_planit_session'
